10204cff6f45090b94bd0762082b879b3acff994
alassane der <alassaneibrahimder@gmail.com>
2024-09-06 23:25:20 +0200

    Initial Commit

---
+++ Vitesse/VitesseApp.swift
@@ -0,0 +1,17 @@
+//
+//  VitesseApp.swift
+//  Vitesse
+//
+//  Created by Alassane Der on 06/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct VitesseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
